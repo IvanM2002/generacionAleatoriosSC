fde72e74477bbd4a4548b1de0204d7719b770bd8
nicolasllanos21 <nicolas.llanos@uptc.edu.co>
2024-03-11 20:19:06 -0500

    se crea metodo para generar numeros con rangos

--- .venv/cuadradosMedios.py
+++ .venv/cuadradosMedios.py
@@ -4,3 +4,12 @@ def cuadrados_medios(semilla, n):
         semilla = int(str(semilla ** 2).zfill(8)[2:6])
         numeros.append(semilla / 10000)
     return numeros
+
+def cuadrados_medios_range(semilla, min, max, n):
+    result = []
+    numeros = cuadrados_medios(semilla, n);
+    for numero in numeros:
+        result.append((int)(numero * ((max-min)+1) + min))
+    return result
+
+print(cuadrados_medios_range(3456, 4,7, 100))
