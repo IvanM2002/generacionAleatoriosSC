713042eb4cc562b68f105d9090b586db0f6102c8
nicolasllanos21 <nicolas.llanos@uptc.edu.co>
2024-03-11 19:31:10 -0500

    Creacion Normal y Uniforme

---
+++ .venv/generador_normal.py
@@ -0,0 +1,60 @@
+import random
+from scipy.stats import norm
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parámetros del generador lineal congruencial
+a = 1664525                         #Pendiente  = {a E Z | 1 <= a < m}
+c = 1013904223                      #Intersección = {c E Z | 0 <= C < m} 
+m = 2**32                           #Conjunto = {m E Z | m > 0} 
+semilla = 1                         #Semilla -> [0,m)  = {Xo E Z | 0 <= Xo < m}
+ri = []                             #Numeros Ri
+
+
+# Lista de números pseudoaleatorios de convolución
+numeros_pseudoaleatoriosMedDes = [15.68866392,17.08559662,17.15452055,15.15261532,17.63585243,17.10922827,16.64418793,
+                                  16.83252325,15.46909741,17.43582994,15.87918557,16.04895496,16.12241714,17.51470964,17.89848077,]
+
+# Parámetros de la distribución normal
+media = np.mean(numeros_pseudoaleatoriosMedDes)
+desviacion_estandar = np.std(numeros_pseudoaleatoriosMedDes)
+ni = []                                                             #Numeros Ni
+
+
+# Función para generar Ri con congruencia Lineal
+def generar_ri(semilla,n):   
+    
+    if semilla is None:
+        semilla = random.randint(0, m - 1)  #Semilla -> [0,m)  = {Xo E Z | 0 <= Xo < m} se utiliza unicamente para generar una semilla si no se especifica una
+        
+    for _ in range(n):
+        semilla = (a * semilla + c) % m
+        ri.append(semilla / (m - 1))    
+
+
+def generarDistrNormal(semilla,n):
+    global ni
+    generar_ri(semilla,n)
+    ni += [norm.ppf(numero, loc=media, scale=desviacion_estandar) for numero in ri]
+    # Calcular la inversa de la distribución normal para cada número pseudoaleatorio
+    return ni
+
+def generar_grafica():        
+
+    # Definir los intervalos para el histograma
+    bins = np.arange(min(ni), max(ni) + 0.1, 0.1)
+
+    # Crear el histograma
+    plt.hist(ni, bins=bins, edgecolor='black')
+
+    # Configurar etiquetas y título
+    plt.xlabel('Valor Inverso (ni)')
+    plt.ylabel('Frecuencia')
+    plt.title('Histograma de Frecuencia para Valores Inversos')
+
+    # Mostrar el histograma
+    plt.show()
+
+
+generarDistrNormal(semilla,50000)
+generar_grafica()
---
+++ .venv/generador_uniforme.py
@@ -0,0 +1,63 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Parámetros del generador con distribucion Uniforme 
+maxI = 10                            #Intervalo superior
+minI = 0                             #Intervalo Inferior
+ni = []                              #Numeros Ni
+nuIntervalos = 8                     #Numero de intervalos para la grafica
+
+# Parámetros del generador lineal congruencial
+a = 1664525                         #Pendiente  = {a E Z | 1 <= a < m}
+c = 1013904223                      #Intersección = {c E Z | 0 <= C < m} 
+m = 2**32                           #Conjunto = {m E Z | m > 0} 
+semilla = 10                         #Semilla -> [0,m)  = {Xo E Z | 0 <= Xo < m}
+ri = []                             #Numeros Ri
+
+
+# Función para generar Ri con congruencia Lineal
+def generar_ri(semilla,total):   
+    
+    if semilla is None:
+        semilla = random.randint(0, m - 1)  #Semilla -> [0,m)  = {Xo E Z | 0 <= Xo < m} se utiliza unicamente para generar una semilla si no se especifica una
+        
+    for _ in range(total):
+        semilla = (a * semilla + c) % m
+        ri.append(semilla / (m - 1))                  
+        
+        
+# Función para generar Ni con distribucion Uniforme
+def generarDistrUniforme(ri, maxIntervalo,minIntervalo,semilla,total):
+    global ni
+    generar_ri(semilla,total)
+    ni += [(minIntervalo + (maxIntervalo - minIntervalo) * N) for N in ri]
+    return ni 
+    
+
+def generar_histograma(num_intervalos):
+    """Genera un histograma de frecuencia por intervalo."""
+    # Calcular los intervalos
+    intervalos = np.linspace(minI, maxI, num=num_intervalos+1)
+
+    # Crear histograma de frecuencia
+    frecuencia, bordes = np.histogram(ni, bins=intervalos)
+
+    # Imprimir los resultados
+    print("Intervalos:", bordes)
+    print("Frecuencia en cada intervalo:", frecuencia)
+
+    # Crear el gráfico de barras
+    plt.bar(range(len(frecuencia)), frecuencia, edgecolor='black')
+
+    # Etiquetas y título del gráfico
+    plt.xlabel('Intervalos')
+    plt.ylabel('Frecuencia')
+    plt.title('Histograma de Frecuencia por Intervalo')
+
+    # Mostrar el gráfico
+    plt.show()
+    
+    
+generarDistrUniforme(ri,maxI,minI,semilla,10000)
+generar_histograma(nuIntervalos)
