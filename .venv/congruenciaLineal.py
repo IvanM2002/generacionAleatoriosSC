fee7015d6bec845b00f665e5e35e20402661c822
IvanM2002 <joivmaji@gmail.com>
2024-03-11 21:54:01 -0500

    se crea metodo para generar numeros con rangos en metodos de congruencia

--- .venv/congruenciaLineal.py
+++ .venv/congruenciaLineal.py
@@ -1,10 +1,15 @@
 def congruencia_lineal(semilla, a, c, m, n):
     numeros = []
-    print(a, m)
     for _ in range(n):
         semilla = (a * semilla + c) % m
         numeros.append(semilla / (m - 1))
     return numeros
+def congruencia_lineal_range(semilla, a,c,m,n, min, max):
+    result = []
+    numeros = congruencia_lineal(semilla, a,c,m,n);
+    for numero in numeros:
+        result.append((int)(numero * ((max-min)+1) + min))
+    return result
 
 
 semilla = 1  # Semilla inicial
@@ -13,4 +18,4 @@ c = 3  # Intercepto
 m = 128  # campo numerico
 n = 10  # Cantidad de números aleatorios a generar
 numeros_aleatorios = congruencia_lineal(semilla, a, c, m, n)
-print(numeros_aleatorios)
+print(congruencia_lineal_range(semilla, a, c, m, n, min=10,max=15))
